c60cfbc725547238899b8ee62281a3d56d064051
Kittu <bharath72kumar@gmail.com>
2018-02-14 12:28:21 +0530

    initial commit

---
+++ mail_outlook.py
@@ -0,0 +1,20 @@
+import win32com.client
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+inbox = outlook.GetDefaultFolder(6)
+messages = inbox.Items
+print(len(messages))
+message = messages.GetLast()
+body_content = message.body
+print(body_content)
+list1 = []
+#all the fields are self explanatory 
+for i in range(len(messages)):
+    print(messages[i].CreationTime)
+    print(messages[i].Subject)
+    print(messages[i].body)
+    print(messages[i].attachments)
+    print(messages[i].SenderName)
+    print(messages[i].Sender)
+    print(messages[i].ReceivedTime)
+
+
